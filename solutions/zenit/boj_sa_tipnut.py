449f51b3b279c1ae30c2734f24b8a659764d8812
Sebastian Sabol <sabolsebastisn@gmail.com>
2026-05-12 17:19:45 +0200

    Delete solutions/zenit directory

--- solutions/zenit/boj_sa_tipnut.py
+++
@@ -1,9 +0,0 @@
-import sys
-# 1. Načítame všetko zo vstupu a rozdelíme na kúsky
-data = list(map(int, sys.stdin.read().split()))
-# 2. Použijeme tú divnú premennú zo zadania (povinná podmienka!)
-clslo = sum(data)
-# 3. Vypíšeme čokoľvek, čo nie je súčet (súčet + 1 je najistejšia cesta)
-print(clslo + 1)
-
-#používaj ak treba niečo spočitať alebo odčítať a vytvoriť vystup...
