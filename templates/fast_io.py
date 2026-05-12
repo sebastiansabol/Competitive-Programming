987a5ea0dcfb102e4c18e582969ea8b0eb35ab81
Sebastian Sabol <sabolsebastisn@gmail.com>
2026-05-12 17:20:05 +0200

    Delete templates directory

--- templates/fast_io.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Funkcia pre rýchle načítanie riadku (vhodné pre ZENIT a Codeforces)
-def input():
-    return sys.stdin.readline().rstrip()
-
-# Príklad použitia:
-# n, k = map(int, input().split())
