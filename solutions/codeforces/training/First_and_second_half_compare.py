5ff988b18e35627b81e8983fae2888b1fc61731c
Sebastian Sabol <sabolsebastisn@gmail.com>
2026-07-08 08:21:08 +0200

    Create First_and_second_half_compare.py

---
+++ solutions/codeforces/training/First_and_second_half_compare.py
@@ -0,0 +1,7 @@
+n = int(input())
+for _ in range(n):
+        s = input()
+        if int(s[0]) + int(s[1]) + int(s[2]) == int(s[3]) + int(s[4]) + int(s[5]):
+                print("YES")
+        else:
+                print("NO")
