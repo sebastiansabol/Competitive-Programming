29e29feaaa0a919f1d61fe6b902947f645940730
Sebastian Sabol <sabolsebastisn@gmail.com>
2026-07-09 08:50:13 +0200

    Create Vasya the Hipster implementation, math.py

---
+++ solutions/codeforces/training/Vasya the Hipster implementation, math.py
@@ -0,0 +1,7 @@
+a, b = list(map(int, input().split()))
+prve = min(a, b)
+print(prve, (a + b - 2 * prve) // 2)
+
+
+
+We have 2 lists of socks, we need to print how many different pairs we can wear each day and the second number is how many same pairs we can wear.
