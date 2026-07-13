e74030ea83f602fba3ae787d736c94aa44765f9e
Sebastian Sabol <sabolsebastisn@gmail.com>
2026-07-13 08:37:25 +0200

    Create Longest_score.py

---
+++ solutions/codeforces/training/Longest_score.py
@@ -0,0 +1,16 @@
+n = int(input())
+for _ in range(n):
+        s = int(input())
+        a = list(map(int, input().split()))
+        score = 0
+        maximum = 0
+        for i in a:
+                if i == 1:
+                        maximum = max(maximum, score)
+                        score = 0
+                elif i == 0:
+                        score += 1
+                        max(maximum, score)
+        print(maximum)
+                        
+                
